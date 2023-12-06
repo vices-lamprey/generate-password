5289501c61e1597535a2dd5a5cb341bca50b00d5
Giovanni Mo <153077000+vices-lamprey@users.noreply.github.com>
2023-12-06 17:18:07 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php
+
+define('PASSWORD_LENGTH', 12);
+define('INCLUDE_UPPERCASE', true);
+define('INCLUDE_LOWERCASE', true);
+define('INCLUDE_DIGITS', true);
+define('INCLUDE_SPECIAL_CHARS', true);
